ed2ba6c15d56fa9cc2b6137104661745f055018b
Aman10881 <129946187+Aman10881@users.noreply.github.com>
2024-01-27 09:48:00 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let score="44"
+// console.log(typeof(score));
+// let valueInstring=Number(score);
+// console.log(typeof valueInstring);
+// console.log(valueInstring);
+
+/*
+conversion 
+"44"=>44
+"44abc"=>NaN
+true =>1; false=>0;  
+*/
+
+// let loggedIn =""
+// let booleanISlogedin=Boolean(loggedIn)
+// console.log(booleanISlogedin);
+
+//  1       => true
+//  ""      => false
+// "aman" => true
+
+let stringNumber = 44
+let stringconvert = String(stringNumber);
+console.log(typeof stringconvert);
+console.log(stringconvert);
