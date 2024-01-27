6dd6df1f3c7bc7fd6ebfb1bc72ce9d11a82cc0ce
Aman10881 <129946187+Aman10881@users.noreply.github.com>
2024-01-27 07:51:21 +0000

    Datatypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //for all JS code for newer version.
+
+// alter(3 + 3); This will not work because we are using nodejs not brower 
+
+let name="aman"
+let age=20
+let isLoogedIN=false
+let state
+
+// Data types-->
+// number =>range 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol=>unquie
+
+
+// object
+
+// console.log(typeof "aman");
+// console.log(typeof age);
+// console.log(typeof 18);
+// console.log(typeof isLoogedIN);
+// console.log(state);
+console.log(typeof null); // null type is object
+console.log(typeof undefined); // undefined type is undefined
