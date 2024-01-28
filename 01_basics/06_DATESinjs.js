7952fe7947b18a634a15b2d404a891ed952f99b3
Aman10881 <129946187+Aman10881@users.noreply.github.com>
2024-01-28 07:34:53 +0000

    date

---
+++ 01_basics/06_DATESinjs.js
@@ -0,0 +1,24 @@
+// DATES 
+
+let mydate = new Date();
+//console.log(typeof mydate); // object
+
+
+// console.log(mydate); // 2024-01-28T07:15:32.645Z
+// console.log(mydate.toString()); // Sun Jan 28 2024 07:16:13 GMT+0000 (Coordinated Universal Time)
+// console.log(mydate.toDateString()); // Sun Jan 28 2024
+// console.log(mydate.toISOString()); // 2024-01-28T07:17:45.900Z
+// console.log(mydate.toJSON());  // 2024-01-28T07:17:45.900Z
+// console.log(mydate.toLocaleDateString()); // 1/28/2024
+// console.log(mydate.toLocaleString()); // 1/28/2024, 7:17:45 AM
+
+
+// let createowndate = new Date(2024,0,14)
+// console.log(createowndate.toDateString()); // Sun Jan 14 2024
+
+// let createowndate = new Date(2024,0,14,5,6)
+//  console.log(createowndate.toLocaleString()); // 1/14/2024, 5:06:00 AM
+
+let createowndate = new Date("11-14-2024")
+// console.log(createowndate.toLocaleString()); // 11/14/2024, 12:00:00 AM
+
